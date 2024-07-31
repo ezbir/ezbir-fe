de674f4391bb0778ae0239dd0953cbdf0b3d2244
IlyaOlkh <ilyaolforgit@gmail.com>
2024-07-31 13:07:26 +0300

    design changed, styles need verification

--- src/components/ChangeableAvatar.tsx
+++ src/components/ChangeableAvatar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, MouseEvent, useState } from "react";
 import Avatar from "antd/es/avatar/avatar";
 import axios from "axios";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -19,17 +19,23 @@ const ChangeableAvatar: React.FC<IChangeableAvatarProps> = ({ link, size }) => {
     const { register, handleSubmit, setValue } = useForm<IFormInput>();
     const [imgUrl, setImgUrl] = useState<string>(link && link !== "undefined" ? link : defaultUrl);
     const [fileBlob, setFileBlob] = useState<Blob | null>(null);
+    const [isFileInput, setIsFileInput] = useState<boolean>(false);
 
     const handleFileChange = (event: FormEvent<HTMLInputElement>) => {
         const files = event.currentTarget.files;
         if (files) {
             const file = files[0];
             const imageUrl = URL.createObjectURL(file);
+            setIsFileInput(true)
             setImgUrl(imageUrl);
-            console.log(imageUrl)
-
             setFileBlob(file);
         }
+    };
+
+    const handleReset = (event: MouseEvent<HTMLButtonElement>) => {
+        setImgUrl(link && link !== "undefined" ? link : defaultUrl)
+        setFileBlob(null)
+        setIsFileInput(false)
     };
 
     const onSubmit: SubmitHandler<IFormInput> = async () => {
@@ -48,6 +54,7 @@ const ChangeableAvatar: React.FC<IChangeableAvatarProps> = ({ link, size }) => {
             }
         })
             .then(response => {
+                console.log("http://13.60.12.224:8080/api/user/upload-picture response:", response.data)
                 if (response.status === 200) {
                     console.log(response);
                     console.log('Avatar uploaded successfully');
@@ -62,6 +69,8 @@ const ChangeableAvatar: React.FC<IChangeableAvatarProps> = ({ link, size }) => {
             .then(response => {
                 if (response.status === 200) {
                     window.sessionStorage.setItem('photoUrl', response.data.photo_url)
+                    setFileBlob(null)
+                    setIsFileInput(false)
                     console.log('Avatar link set successfully');
                 } else {
                     console.error('Error getting user data');
@@ -70,10 +79,11 @@ const ChangeableAvatar: React.FC<IChangeableAvatarProps> = ({ link, size }) => {
             .catch(error => {
                 console.error('Error getting user data', error);
             });
+
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form className="relative" onSubmit={handleSubmit(onSubmit)}>
             <label className="cursor-pointer" htmlFor="input_avatar_img">
                 <input type="file"
                     accept="image/*"
@@ -83,8 +93,14 @@ const ChangeableAvatar: React.FC<IChangeableAvatarProps> = ({ link, size }) => {
                     {...register("picture")}
                 />
                 <Avatar shape="square" size={size} icon={<img src={imgUrl} alt="avatar" />} />
+                <div hidden={isFileInput} className="bg-yellow-500 text-white rounded absolute bottom-[-0.3125rem] left-[-0.3125rem] w-10 h-10 text-2xl flex justify-center items-center">
+                    <img src="img/pencilIcon.svg" className="w-7 h-7" />
+                </div>
             </label>
-            <button type="submit">Відправити</button>
+
+
+            <button onClick={handleReset} hidden={!isFileInput} className="bg-red-500 text-white rounded absolute bottom-[-0.3125rem] left-[-0.3125rem] w-10 h-10 text-2xl" type="reset">Х</button>
+            <button hidden={!isFileInput} className="bg-green-500 text-white rounded absolute bottom-[-0.3125rem] right-[-0.3125rem] w-10 h-10 text-2xl" type="submit">✔</button>
         </form>
     );
 };
