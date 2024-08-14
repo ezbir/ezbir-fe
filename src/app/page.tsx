442648e2226719f72cba17037626bf71bb7fb369
ruslan_kuchmynda <ruslankuchmynda@gmail.com>
2024-08-14 14:25:53 +0300

    29-make-an-adaptive-design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,8 @@ export default function Home() {
     <>
         <Header/>
         <main>
-            <RussianWarshipRIP/>
+            {/*todo create new RussianRip*/}
+            {/*<RussianWarshipRIP/>*/}
         </main>
         <Footer/>
     </>
